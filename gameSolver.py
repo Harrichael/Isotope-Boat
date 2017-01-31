26fd82941f0da3054c38cae71c57f2a2383e492c
Michael Harrington <surfer.of.cloud@gmail.com>
2017-01-30 19:12:22 -0600

    actually added gamesolver

---
+++ gameSolver.py
@@ -0,0 +1,41 @@
+"""
+Michael Harrington
+
+This file exposes the game solver class that solves the puzzle
+"""
+
+from timer import timeStampMS
+from readPuzzleInput import getStateFromFile
+from gameRules import neighborGen, isGoalState, costCalc
+from pathFinders import BFTS
+
+"""
+GameSolver class handles extracting file input, searching, and printing
+"""
+class GameSolver():
+    def __init__(self):
+        pass
+
+    def runInputFile(self, inputFilePath):
+        initialState = getStateFromFile(inputFilePath)
+
+        startTime = timeStampMS()
+        self.pathSolver = BFTS(initialState, neighborGen, costCalc, isGoalState)
+        endTime = timeStampMS()
+        self.totalTime = endTime - startTime
+
+        return self.pathSolver.pathFound
+
+    def strOutput(self):
+        path = self.pathSolver.actionPath
+        finalSearchState = self.pathSolver.searchNodePath[-1]
+        finalState = finalSearchState.boardState
+        actions = self.pathSolver.actionPath
+        pathCost = 0 if finalSearchState is None else finalSearchState.parent.pathCost
+
+        return '\n'.join(map(str, [ self.totalTime,
+                                    pathCost,
+                                    len(actions),
+                                    ','.join(map(str, actions)),
+                                    finalState
+                                  ]))
