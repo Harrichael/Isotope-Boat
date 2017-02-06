6c048ec9d3d085c7e62a5ad9a2c134211f7df30f
Michael Harrington <surfer.of.cloud@gmail.com>
2017-02-05 19:52:36 -0600

    Program computes two files

--- main.py
+++ main.py
@@ -10,12 +10,21 @@ addition of a gradient cost field dubbed a radiation field.
 
 from game.gameSolver import GameSolver
 
+inputPuzzles = [ 'puzzle1.txt',
+                 'examplePuzzle.txt',
+               ]
+
+outputFiles = [ 'solution1.txt',
+                'solution2.txt',
+              ]
+
 if __name__ == '__main__':
     solver = GameSolver()
-    if solver.runInputFile('puzzles/puzzle1.txt'):
-        output = solver.strOutput()
-        with open('solutions/solution1.txt', 'w') as fileObj:
-            fileObj.write(output)
-    else:
-        print '(Error) No solution found'
+    for inFile, outFile in zip(inputPuzzles, outputFiles):
+        if solver.runInputFile('puzzles/' + inFile):
+            output = solver.strOutput()
+            with open('solutions/' + outFile, 'w') as fileObj:
+                fileObj.write(output)
+        else:
+            print '(Error) No solution found'
 
